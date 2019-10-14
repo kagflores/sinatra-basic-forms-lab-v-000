5089c04ebad206319a4939a103230b32da32fcbb
Karen Flores <karen.ag.flores@gmail.com>
2019-10-14 19:23:42 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ class App < Sinatra::Base
   post '/puppy' do
     @name = params["name"]
     @breed = params["breed"]
-    @age = params["age"]
+    @months_old = params["months_old"]
     erb :display_puppy
   end
 
